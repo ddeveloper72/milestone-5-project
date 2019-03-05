a5689d5248abc9a475dd15c74ac26db30cad8273
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-05 22:26:38 +0000

    Removed secretkey to os.environ

--- drone_debug/settings.py
+++ drone_debug/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$szdz&mx_$0-ps)%ug$dke+5q&^&&ima0@^fiao$z!h(hpl*vz'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

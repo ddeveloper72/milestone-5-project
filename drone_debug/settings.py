ba6485ee9be6dd1026062eb5fad78bfdcf4ee0ed
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-06 22:42:09 +0000

    Reinstated development secret key to test travis

--- drone_debug/settings.py
+++ drone_debug/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '$szdz&mx_$0-ps)%ug$dke+5q&^&&ima0@^fiao$z!h(hpl*vz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development

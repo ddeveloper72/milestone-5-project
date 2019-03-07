8a8e8d5dc4e05378014887ef8c7c442fe0864781
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-07 21:59:45 +0000

    Imported Auth app - Converting urls.py WIP

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import include, re_path
+from accounts.views import logout, login, registration, user_profile
+from accounts import url_reset
+
+urlpatterns = [
+    re_path(r'^logout/', logout, name="logout"),
+    re_path(r'^login/', login, name="login"),
+    re_path(r'^register/', registration, name="registration"),
+    re_path(r'^profile/', user_profile, name="profile"),
+    re_path(r'^password-reset/', include(url_reset))
+]

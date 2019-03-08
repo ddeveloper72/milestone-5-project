f0a5eab7b476b4db6a99963255521c2bb45e81ab
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-08 00:09:32 +0000

    Reconfigured urlspatterns from django 1.11 to 2.1.7, applying views to patterns.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,9 @@
-from django.urls import include, re_path
-from accounts.views import logout, login, registration, user_profile
-from accounts import url_reset
+from django.urls import path
+from accounts import views
 
 urlpatterns = [
-    re_path(r'^logout/', logout, name="logout"),
-    re_path(r'^login/', login, name="login"),
-    re_path(r'^register/', registration, name="registration"),
-    re_path(r'^profile/', user_profile, name="profile"),
-    re_path(r'^password-reset/', include(url_reset))
+    path('logout/', views.logout, name="logout"),
+    path('login/', views.login, name="login"),
+    path('register/', views.registration, name="registration"),
+    path('profile/', views.user_profile, name="profile"),
 ]

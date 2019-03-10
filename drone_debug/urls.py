a66a13e318d7dccc8f54f4c2021e0466bb857d6b
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-10 16:33:23 +0000

    Added new url path for favicon

--- drone_debug/urls.py
+++ drone_debug/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views import static
 
 
 urlpatterns = [
+    path('favicon.ico/', static.serve, ('favicon/favicon.ico'), name="favicon"),
     path('', include('landing_page.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),

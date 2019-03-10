c01ba8a4d4f0a8e659c09ef4a40e49b534866b63
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-10 22:59:20 +0000

    Updated static files url and fsvicon path

--- drone_debug/urls.py
+++ drone_debug/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.views import static
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
+favicon_view = RedirectView.as_view(url='favicon/favicon.ico', permanent=True)
 
 urlpatterns = [
-    path('favicon.ico/', static.serve, ('favicon/favicon.ico'), name="favicon"),
+    path('favicon.ico/', favicon_view, name="favicon"),
     path('', include('landing_page.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

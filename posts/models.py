d450ebe05e85e7908b14a096c2950fc4fa7c89bc
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-12 23:59:37 +0000

    Updated blog model to include comment WIP

--- posts/models.py
+++ posts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Post(models.Model):
     """
     A single blog post
@@ -12,12 +13,28 @@ class Post(models.Model):
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(blank=True, null=True,
-                                            default=timezone.now)
+                                        default=timezone.now)
     views = models.IntegerField(default=0)
     tag = models.CharField(max_length=30, blank=True, null=True)
     image = models.ImageField(upload_to="img", blank=True, null=True)
-    author = models.ForeignKey(User, default=None)
+    author = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
 
     def __unicode__(self):
         return self.title
 
+
+class Comment(models.Model):
+    """
+    A comment following a blog post
+    """
+    comment = models.TextField(blank=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User, default=None, related_name="comment_author", 
+        on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, default=None, on_delete=models.CASCADE)
+    is_reported = models.BooleanField(default=False)
+    is_hidden = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.post.title

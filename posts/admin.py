d85434f3bc5ad884266236fa96b093fc7f91fd92
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-12 22:29:06 +0000

    Imported blog admin.py, added a new model Comment for future WIP

--- posts/admin.py
+++ posts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Post, PostComment
+from .models import Post, Comment
 
 # Register your models here.
 
 admin.site.register(Post)
-admin.site.register(PostComment)
+admin.site.register(Comment)

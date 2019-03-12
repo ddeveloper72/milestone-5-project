e3b6b4f9bcf7ca449c21808d84093cc65c66d764
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-12 20:00:52 +0000

    Added auth_index for wiring in an authoized index home page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from accounts.views import logout, login, registration, user_profile
+from accounts.views import logout, login, registration, auth_index, user_profile
 
 urlpatterns = [
     path('logout/', logout, name="logout"),
     path('login/', login, name="login"),
     path('register/', registration, name="registration"),
+    path('auth_index/', auth_index, name="auth_index"),
     path('profile/', user_profile, name="profile"),
     path('reset/', include('accounts.url_reset')),
 ]

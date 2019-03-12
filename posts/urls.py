2262e2e2025be11953abca8da9152bc34305845f
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-12 23:00:52 +0000

    Used re_repath to update blog urls WIP

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, include
+from django.urls import path, re_path
 from posts.views import get_posts, post_detail, create_or_edit_a_post
 
 # CRUD operation url templates
 urlpatterns = [
     path('', get_posts, name='get_posts'), 
-    path('(?P<pk>\d+)$', post_detail, name='post_detail'), 
-    path('new/$', create_or_edit_a_post, name='new_post'),
-    path('(?P<pk>\d+)/edit/$', create_or_edit_a_post, name='edit_post'path),
+    re_path(r'^(?P<pk>\d+)$', post_detail, name='post_detail'), 
+    re_path(r'^new/$', create_or_edit_a_post, name='new_post'),
+    re_path(r'^(?P<pk>\d+)/edit/$', create_or_edit_a_post, name='edit_post')
 ]

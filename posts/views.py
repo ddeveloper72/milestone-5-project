e5e2255e9958bf11172e2cf20f8e618cd37929c1
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-14 22:41:28 +0000

    Added a view for posting a comment.

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
-from .forms import BlogPostForm
+from .forms import BlogPostForm, CommentForm
 
 
 # Create your views here.
@@ -46,10 +46,21 @@ def create_or_edit_a_post(request, pk=None):
         form = BlogPostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             post = form.save()
-            return redirect(post_detail, post.pk)
-        
+            return redirect(post_detail, post.pk)        
     else:
         form = BlogPostForm(instance=post) 
     return render(request, 'blogpostform.html', {'form': form})
 
 
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'add_comment_to_post.html', {'form': form})

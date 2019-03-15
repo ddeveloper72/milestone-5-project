99cd63bb6f0b582265fe094014f2b07c1cbf139c
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-15 13:44:38 +0000

    Added url patters for wiring the approve or remove buttons to the views functions.

--- posts/urls.py
+++ posts/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('', get_posts, name='get_posts'), 
     path('post/<int:pk>/comment/', add_comment_to_post, 
          name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
     re_path(r'^(?P<pk>\d+)$', post_detail, name='post_detail'), 
     re_path(r'^new/$', create_or_edit_a_post, name='new_post'),
     re_path(r'^(?P<pk>\d+)/edit/$', create_or_edit_a_post, name='edit_post')

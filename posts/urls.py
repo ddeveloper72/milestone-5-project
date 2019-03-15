7f9c0cd555d16b5a714d4d8c6ca103c33409eb05
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-15 21:28:15 +0000

    Refactored urlpatterns .

--- posts/urls.py
+++ posts/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path, re_path
-from posts.views import get_posts, post_detail, create_or_edit_a_post, \
-    add_comment_to_post, comment_approve, comment_remove
+from posts import views
 
 # CRUD operation url templates
 urlpatterns = [
-    path('', get_posts, name='get_posts'), 
-    path('post/<int:pk>/comment/', add_comment_to_post, 
-         name='add_comment_to_post'),
-    path('comment/<int:pk>/approve/', comment_approve, name='comment_approve'),
-    path('comment/<int:pk>/remove/', comment_remove, name='comment_remove'),
-    re_path(r'^(?P<pk>\d+)$', post_detail, name='post_detail'), 
-    re_path(r'^new/$', create_or_edit_a_post, name='new_post'),
-    re_path(r'^(?P<pk>\d+)/edit/$', create_or_edit_a_post, name='edit_post')
+    path('', views.get_posts,
+        name='get_posts'),
+    re_path(r'^(?P<pk>\d+)$', views.post_detail,
+        name='post_detail'),
+    re_path(r'^new/$', views.create_or_edit_a_post,
+        name='new_post'),
+    re_path(r'^(?P<pk>\d+)/edit/$', views.create_or_edit_a_post,
+        name='edit_post'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post,
+        name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve,
+        name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove,
+        name='comment_remove'),
 ]

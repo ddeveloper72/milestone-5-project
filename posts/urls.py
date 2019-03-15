1accb72bce2ff6b99ddc7356af8f0a13e57e3651
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-15 13:56:04 +0000

    Updated views for comment approve & remove urls

--- posts/urls.py
+++ posts/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, re_path
 from posts.views import get_posts, post_detail, create_or_edit_a_post, \
-    add_comment_to_post
+    add_comment_to_post, comment_approve, comment_remove
 
 # CRUD operation url templates
 urlpatterns = [
     path('', get_posts, name='get_posts'), 
     path('post/<int:pk>/comment/', add_comment_to_post, 
          name='add_comment_to_post'),
-    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
-    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    path('comment/<int:pk>/approve/', comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', comment_remove, name='comment_remove'),
     re_path(r'^(?P<pk>\d+)$', post_detail, name='post_detail'), 
     re_path(r'^new/$', create_or_edit_a_post, name='new_post'),
     re_path(r'^(?P<pk>\d+)/edit/$', create_or_edit_a_post, name='edit_post')

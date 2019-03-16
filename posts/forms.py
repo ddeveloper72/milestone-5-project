c82f0c6bc8e33c13d2bc9c03072c96fe4068e987
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-16 22:40:19 +0000

    Added widgets to the PostForm class to better defind the placeholders.

--- posts/forms.py
+++ posts/forms.py
@@ -7,6 +7,11 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'content', 'image', 'tag', 'published_date')
+        widgets = {
+            'title': forms.TextInput(attrs={'placeholder': 'Post Title'}),
+            'content': forms.Textarea(attrs={'placeholder': 'About your post here...'}),
+            'tag': forms.TextInput(attrs={'placeholder': 'keywords identifying items'})
+        }
 
 
 class CommentForm(forms.ModelForm):

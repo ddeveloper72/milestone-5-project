45af9c08ab731dd7375d7c003eb6066d63767907
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-16 22:41:17 +0000

    Removed the view function names from the imports- getting to long.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path, include
-from accounts.views import logout, login, registration, auth_index, user_profile
+from accounts import views
 
 urlpatterns = [
-    path('logout/', logout, name="logout"),
-    path('login/', login, name="login"),
-    path('register/', registration, name="registration"),
-    path('auth_index/', auth_index, name="auth_index"),
-    path('profile/', user_profile, name="profile"),
+    path('logout/', views.logout, name="logout"),
+    path('login/', views.login, name="login"),
+    path('register/', views.registration, name="registration"),
+    path('profile/', views.user_profile, name="profile"),
     path('reset/', include('accounts.url_reset')),
 ]

bb03388f3729abfc9b6582b2393907b95ed38a80
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-16 19:33:01 +0000

    Updated comments in accounts views.py

--- accounts/views.py
+++ accounts/views.py
@@ -2,84 +2,118 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from accounts.forms import UserloginForm, UserRegistrationForm 
+from accounts.forms import UserloginForm, UserRegistrationForm
 
 # Create your views here.
 
+
 def index(request):
-    """return the index.html file"""
-    
+    """
+    return the index.html file
+    """   
     return render(request, 'index.html')
 
 
 @login_required
 def auth_index(request):
-    """Generate the views for the authorized customer"""
+    """
+    Generate the views for the authorized customer if logged in.
+    """
     user = User.objects.get(email=request.user.email)
     return render(request, 'auth_index.html', {"profile": user})
 
-@login_required # Checks to see if a user is loged in BEFORE running the logout function.   
+
+@login_required
 def logout(request):
-    """Log the user out"""
-    
+    """
+    Log the user out, if the user is already logged in.
+    """    
     auth.logout(request)
     messages.success(request, "You have been successfully logged out!")
     return redirect(reverse('index'))
 
-def login(request):
-    """Return a login page"""
-    if request.user.is_authenticated:
-        return redirect(reverse('index')) 
-        # security which prevents access to the login page via url bar
 
-    if request.method == "POST": 
+def login(request):
+    """
+    Return a login page
+    """
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
+        """
+        security which prevents access to the login page via url bar
+        """     
+
+    if request.method == "POST":
         login_form = UserloginForm(request.POST)
 
         if login_form.is_valid():
-            user =  auth.authenticate(username=request.POST['username'],
+            user = auth.authenticate(username=request.POST['username'],
                                       password=request.POST['password'])
 
             if user:
                 auth.login(user=user, request=request)
                 messages.success(request, "Your login is sucessfull!")
-                return redirect(reverse('auth_index'))
-                # security which prevents access to the login page via url bar
+                return redirect(reverse('index'))
+                """
+                security which prevents access to the login page via url bar
+                """               
 
             else:
-                login_form.add_error(None, "Your username or password is incorrect")
+                login_form.add_error(None,
+                                    "Your username or password is \
+                                    incorrect")
 
     else:
         login_form = UserloginForm()
 
     return render(request, 'login.html', {"login_form": login_form})
 
+
 def registration(request):
-    """Render the registration page"""
+    """
+    Render the registration page
+    """
     if request.user.is_authenticated:
-        return redirect(reverse('index')) # redirect a registerd user away from registration to the index
+        return redirect(reverse('index')) 
+        """
+        redirect a registerd user away from registration to the index
+        """
 
     if request.method == "POST":
-        registration_form = UserRegistrationForm(request.POST)    
+        registration_form = UserRegistrationForm(request.POST)
 
         if registration_form.is_valid():
-            registration_form.save() # add the user data to the User model in the database
+            """
+            add the user data to the User model in the database
+            """
+            registration_form.save()
             user = auth.authenticate(username=request.POST['username'],
-                                    password=request.POST['password1'])
+                                     password=request.POST['password1'])
             if user:
                auth.login(user=user, request=request)
-               messages.success(request, "You have registerd successfully") 
-               return redirect(reverse('index')) # redirect the newly registerd user away from registration to the index
+               messages.success(request, "You have registerd successfully")
+               return redirect(reverse('index'))
+               """
+               redirect the newly registerd user away from registration to 
+               the index
+               """
                   
         else:
-              messages.error(request, registration_form.errors)            
-            # messages.warning(request, "Plesse check that your passwords match and username/email is unique!")              
+            messages.error(request, registration_form.errors)            
+            """
+            messages.warning(request, "Plesse check that your passwords
+            match and username/email is unique!")
+            """        
 
     registration_form = UserRegistrationForm()
     return render(request, 'registration.html', {
         "registration_form": registration_form})
 
+
 def user_profile(request):
-    """The user's profile page"""
+    """T
+    he user's profile page
+    """
     user = User.objects.get(email=request.user.email)
     return render(request, 'profile.html', {"profile": user})
 

7999bfa79565bdc698c0c90d5d525406cb549b24
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-17 16:42:51 +0000

    Css files will only lode if the static-files are located using storages.backends.s3boto3.S3Boto3Storage

--- drone_debug/settings.py
+++ drone_debug/settings.py
@@ -173,7 +173,7 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_DEFAULT_ACL = None
 
 STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 STATIC_URL = '/static/'

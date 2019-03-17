5c73fc5a48cf5d041bf0b2c2365fcf6cc00096a4
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-17 01:32:14 +0000

    Updated pep8 formating

--- posts/models.py
+++ posts/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     content = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(blank=True, null=True,
-                                            default=timezone.now)
+                                          default=timezone.now)
     views = models.IntegerField(default=0)
     tag = models.CharField(max_length=60, blank=True, null=True)
     image = models.ImageField(upload_to="img", blank=True, null=True)
@@ -24,7 +24,7 @@ class Post(models.Model):
 
     def __unicode__(self):
         return self.title
-    
+
     def approved_comments(self):
         return self.comments.filter(approved_comment=True)
 
@@ -39,7 +39,7 @@ class Comment(models.Model):
         User, default=None, related_name="comment_author",
         on_delete=models.CASCADE)
     post = models.ForeignKey(Post, default=None, on_delete=models.CASCADE,
-                                related_name='comments')
+                                   related_name='comments')
     approved_comment = models.BooleanField(default=False)
     is_reported = models.BooleanField(default=False)
     is_hidden = models.BooleanField(default=False)

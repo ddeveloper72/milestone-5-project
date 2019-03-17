0e6ab53c4d91a3b243f525ee840d70fe409cc5d6
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-17 15:28:57 +0000

    Updated tet form to incude test that fails as well as gechls for django's default pw authentication requirements.

--- accounts/test_forms.py
+++ accounts/test_forms.py
@@ -16,7 +16,7 @@ class TestUserloginForm(TestCase):
                                    'password': 'password'})
         self.assertTrue(form.is_valid())
 
-    def test_correct_message_for_missing_required_field(self):
+    def test_for_missing_required_field(self):
         form = UserloginForm({'username': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['username'], [u'This field is required.'])
@@ -26,9 +26,21 @@ class TestUserRegistrationForm(TestCase):
     """
     Test if the user registration formm data is valid
     """
-    def test_UserRegistrationForm(self):
+
+    def test_UserRegistrationForm_valid(self):
         form = UserRegistrationForm(
-            data={'username': 'user', 'password1': 'password',
-                  'password2': 'password',
+            data={'username': 'user', 'password1': '12A@345a',
+                  'password2': '12A@345a',
                   'email': 'someone@domain.ie'})
         self.assertTrue(form.is_valid())
+
+    """
+    Test if the user registration formm data is not valid
+    """
+
+    def test_UserRegistrationForm_invalid(self):
+        form = UserRegistrationForm(
+            data={'username': 'a', 'password1': 'b',
+                  'password2': 'c',
+                  'email': 'd'})
+        self.assertFalse(form.is_valid())

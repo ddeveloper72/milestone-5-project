283673e2aed45b594fecc68c1c9037d0b38eb027
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-17 20:56:22 +0000

    Created test view for get registration page

--- accounts/test_views.py
+++ accounts/test_views.py
@@ -3,10 +3,18 @@ from django.urls import resolve, reverse
 
 
 class TestViews(TestCase):
-    def test_get_home_page(self):
+    def test_get_login_page(self):
         """
-        test for home page, by looking for server code 200
+        test for login page, by looking for server code 200
         """
-        page = self.client.get("/")
+        page = self.client.get(reverse("login"))
         self.assertEqual(page.status_code, 200)
-        self.assertTemplateUsed(page, "index.html")
+        self.assertTemplateUsed(page, "login.html")
+
+    def test_get_registration_page(self):
+        """
+        test for login page, by looking for server code 200
+        """
+        page = self.client.get(reverse("registration"))
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "registration.html")

b6161ed5295cd897bb9420b1c43b1c5876d768c1
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-18 17:26:33 +0000

    Removed UserProfile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-
-    def __str__(self):
-        return str(self.user)

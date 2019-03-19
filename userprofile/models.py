07be87956b571f05f3a63174e83d92bc525a3a68
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-19 23:40:32 +0000

    Added on_delete to onetoone field for user.

--- userprofile/models.py
+++ userprofile/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)

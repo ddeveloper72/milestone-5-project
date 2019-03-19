3b0df77ce7721f39d0c81cb7ea6156b6fc1c8c5d
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-19 23:24:48 +0000

    Added a new view for userprofile

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.models import User
 from forms import UserProfileForm
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 @login_required
 def user_profile(request):
+    user = User.objects.get(email=request.user.email)    
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=request.user.profile)
         if form.is_valid():
@@ -15,4 +17,4 @@ def user_profile(request):
         profile = user.profile
         form = UserProfileForm(instance=profile)
 
-    return render(request, 'profile.html', {'form': form})
+    return render(request, 'profile.html', {'form': form}, {"profile": user})

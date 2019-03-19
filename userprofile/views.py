1af092c9587217e3ef305ff805a0d7a8c1ef4786
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-19 23:17:00 +0000

    Created a vew for the usrerprofile

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, reverse
+from forms import UserProfileForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required
+def user_profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('index'))
+    else:
+        user = request.user
+        profile = user.profile
+        form = UserProfileForm(instance=profile)
+
+    return render(request, 'profile.html', {'form': form})

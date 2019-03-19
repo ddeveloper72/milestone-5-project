5f20465888512d43a39877f778a81b3990a88d89
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-19 23:24:14 +0000

    Removed user profile view from accounts.views.py

--- accounts/views.py
+++ accounts/views.py
@@ -107,18 +107,3 @@ def registration(request):
     registration_form = UserRegistrationForm()
     return render(request, 'registration.html', {
         "registration_form": registration_form})
-
-
-@login_required
-def user_profile(request):
-    """
-    Get the user's profile page
-    """
-    user = User.objects.get(email=request.user.email)
-    return render(request, 'profile.html', {"profile": user})
-
-
-
-    
-
-

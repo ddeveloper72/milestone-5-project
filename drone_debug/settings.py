e019938bb8f37a8d39d5e0476767c20ed7de27a2
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-20 13:43:32 +0000

    Testing a change to app settings for travis.

--- drone_debug/settings.py
+++ drone_debug/settings.py
@@ -105,7 +105,7 @@ else:
             'NAME': 'drone',
             'USER': os.environ.get('USER'),
             'PASSWORD': os.environ.get('PASSWORD'),
-            'HOST': os.environ.get('HOST'),
+            'HOST': os.environ.get('localhost', '127.0.0.1'),
             'PORT': os.environ.get('PORT'),        
             }
     }

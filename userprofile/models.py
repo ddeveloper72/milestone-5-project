34694cdb779685dacc484225f78e385938df34cc
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-20 00:05:49 +0000

    Corrected a typo in user.profile @ (user=u)[0]

--- userprofile/models.py
+++ userprofile/models.py
@@ -9,4 +9,4 @@ class UserProfile(models.Model):
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
 
-User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u[0]))
+User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])

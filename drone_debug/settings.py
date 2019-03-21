1bb09f9227673d0170c66479534e4be03a30d2ac
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-21 23:22:24 +0000

    Corrected pep8 bug picked up by travis

--- drone_debug/settings.py
+++ drone_debug/settings.py
@@ -120,26 +120,22 @@ else:
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.
-        UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.
-        MinimumLengthValidator',
+        'django.contrib.auth.password_validation.MinimumLengthValidator',
         'OPTIONS': {
             'min_length': 4,
         }
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.
-        CommonPasswordValidator',
+        'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.
-        NumericPasswordValidator',
+        'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 

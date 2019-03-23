c9e772132b0b6cca81d0fbd61b0a186b1982549e
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-23 20:44:57 +0000

    Created 1st issue view WIP

--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect, reverse, \
+    HttpResponse
+from django.utils import timezone
+from django.contrib import messages
 from .models import Issue
+from .forms import AddEditIssueFrom
 
 # Create your views here.
 
@@ -9,14 +13,16 @@ from .models import Issue
 def get_issues(request):
     """
     Create a view that will return a list
-    of Posts that were published prior to 'now'
-    and render them to the 'blogpost.html' template.
+    of Issues that were published prior to 'now'
+    and render them to the 'issues_list.html' template.    
     """
     try:
-        issues = IssuesList.objects.filter(
-            published_date__lte=timezone.now()
-            ).order_by('-published_date')
+        issues = Issue.objects.filter(published_date__lte=timezone.now()
+                                      ).order_by('-published_date')
         return render(request, "issues_list.html", {'issues': issues})
+
     except:
         messages.info(request, "There are no issues logged yet")
         return redirect(reverse('get_issues'))
+  
+

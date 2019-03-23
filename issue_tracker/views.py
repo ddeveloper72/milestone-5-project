ac6533a4abdddd87ff5e35175ab7ba38c47cb004
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-23 15:43:10 +0000

    Used makemigrations to help dbug views

--- issue_tracker/views.py
+++ issue_tracker/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from .models import IssuesList
+from .models import Issue
 
 # Create your views here.
 
 
 @login_required
-def get_isues(request):
+def get_issues(request):
     """
     Create a view that will return a list
     of Posts that were published prior to 'now'
     and render them to the 'blogpost.html' template.
     """
     try:
-        issues = IssuesList.objects.filter(published_date__lte=timezone.now()
-                                          ).order_by('-published_date')
+        issues = IssuesList.objects.filter(
+            published_date__lte=timezone.now()
+            ).order_by('-published_date')
         return render(request, "issues_list.html", {'issues': issues})
     except:
         messages.info(request, "There are no issues logged yet")

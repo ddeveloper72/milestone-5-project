47c08228e971d63291b8589ef78321966c7b47a0
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-23 15:02:47 +0000

    1st pass at issue_tracker urls.py WIP

---
+++ issue_tracker/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, re_path
+from issue_tracker import views
+
+
+# CRUD operation url templates
+urlpatterns = [
+    path('', views.get_issues,
+         name='get_posts')
+]

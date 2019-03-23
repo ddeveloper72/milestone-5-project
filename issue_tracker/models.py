2f171ae1d92692205b0d6c66d66d17ded28ff12a
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-23 20:48:13 +0000

    Made the model formating consistant with all other forms already written.

--- issue_tracker/models.py
+++ issue_tracker/models.py
@@ -11,40 +11,24 @@ class Issue(models.Model):
     The issues list needs allows a user to raise an issue for the
     development team to review.
     """
-    title = models.CharField(
-        max_length=140)
-    created_date = models.DateTimeField(
-        blank=True,
-        null=True,
-        default=timezone.now)
-    completed = models.BooleanField(
-        default=False)
-    completed_date = models.DateTimeField(
-        blank=True,
-        null=True)
-    created_by = models.ForeignKey(
-        User,
-        null=True,
-        on_delete=models.CASCADE,
-    )
-    content = models.TextField(
-        blank=True,
-        null=True)
+    title = models.CharField(max_length=140)
+    created_date = models.DateTimeField(blank=True, null=True,
+                                        default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True,
+                                          default=timezone.now)
+    completed = models.BooleanField(default=False)
+    completed_date = models.DateTimeField(blank=True, null=True)
+    content = models.TextField(blank=True, null=True)
     views = models.IntegerField(default=0)
-    tag = models.CharField(
-        max_length=60,
-        blank=True,
-        null=True)
-    image = models.ImageField(
-        upload_to="img",
-        blank=True,
-        null=True)
+    tag = models.CharField(max_length=60, blank=True, null=True)
+    image = models.ImageField(upload_to="img", blank=True, null=True)
+    author = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
     priority = models.PositiveIntegerField()
 
     class Meta:
         ordering = ['-created_date']
 
-    def __str__(self):
+    def __unicode__(self):
         return self.title
 
 
@@ -56,8 +40,7 @@ class Comment(models.Model):
         User, 
         on_delete=models.CASCADE,
         blank=True,
-        null=True
-    )
+        null=True)
     date = models.DateTimeField(
         default=datetime.datetime.now)
     comment = models.TextField(

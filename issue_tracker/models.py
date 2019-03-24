0e71df8230b9d93a99bc381d2460ecdaf199893c
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-24 00:21:39 +0000

    Refactored issues models

--- issue_tracker/models.py
+++ issue_tracker/models.py
@@ -12,8 +12,7 @@ class Issue(models.Model):
     development team to review.
     """
     title = models.CharField(max_length=140)
-    created_date = models.DateTimeField(blank=True, null=True,
-                                        default=timezone.now)
+    created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(blank=True, null=True,
                                           default=timezone.now)
     completed = models.BooleanField(default=False)
@@ -22,7 +21,8 @@ class Issue(models.Model):
     views = models.IntegerField(default=0)
     tag = models.CharField(max_length=60, blank=True, null=True)
     image = models.ImageField(upload_to="img", blank=True, null=True)
-    author = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
+    issue_author = models.ForeignKey(User, default=None,
+                                     on_delete=models.CASCADE)
     priority = models.PositiveIntegerField()
 
     class Meta:
@@ -36,23 +36,19 @@ class Comment(models.Model):
     """
     Like the blog, a user can comment on an issue posted by another user.
     """
+    comment = models.TextField(blank=False)
+    created_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
-        User, 
-        on_delete=models.CASCADE,
-        blank=True,
-        null=True)
-    date = models.DateTimeField(
-        default=datetime.datetime.now)
-    comment = models.TextField(
-        blank=False)
-    issue = models.ForeignKey(
-        Issue, 
-        default=None,
+        User, default=None, related_name="issue_comment_author",
         on_delete=models.CASCADE)
-    is_reported = models.BooleanField(
-        default=False)
-    is_hidden = models.BooleanField(
-        default=False)
+    issue = models.ForeignKey(Issue, default=None, on_delete=models.CASCADE)
+    approved_comment = models.BooleanField(default=False)
+    is_reported = models.BooleanField(default=False)
+    is_hidden = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
 
     def __str__(self):
         return self.comment

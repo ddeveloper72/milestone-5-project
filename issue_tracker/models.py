d72f56150c82e7dc16940c82d4888b599765cc31
ddeveloper72 <duncan.niamh@gmail.com>
2019-03-24 01:23:12 +0000

    Added approved_comment to Issue model

--- issue_tracker/models.py
+++ issue_tracker/models.py
@@ -31,6 +31,9 @@ class Issue(models.Model):
     def __unicode__(self):
         return self.title
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
 
 class Comment(models.Model):
     """

5834374768032e4880a1100a189cba8584c4ef7b
ddeveloper72 <duncan.niamh@gmail.com>
2019-04-06 23:28:42 +0100

    Corrected imported cart views typo

--- cart/urls.py
+++ cart/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path
 from cart import views
 
 urlpatterns = [
-    path('', view.view_cart, name='view_cart'),
-    re_path(r'^add/(?P<id>\d+)', vieww.add_to_cart, name='add_to_cart'),
-    re_path(r'^adjust/(?P<id>\d+)', view.adjust_cart, name='adjust_cart'),
+    path('', views.view_cart, name='view_cart'),
+    re_path(r'^add/(?P<id>\d+)', views.add_to_cart, name='add_to_cart'),
+    re_path(r'^adjust/(?P<id>\d+)', views.adjust_cart, name='adjust_cart'),
     ]

6d92993bc61f2996478190342a0d81d390cb3bf7
ddeveloper72 <duncan.niamh@gmail.com>
2019-04-06 23:32:07 +0100

    Updated view WIP to generate cart items

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
+@login_required
 def view_cart(request):
     """
     A view that renders the cart contents page
@@ -10,19 +12,21 @@ def view_cart(request):
     return render(request, "cart.html")
 
 
+@login_required
 def add_to_cart(request, pk):
     """
     Add a quantity of the specified product to the cart
     """
-    quantity = int(request.POST.get('quantity'))    
+    quantity = int(request.POST.get('quantity'))
 
     cart = request.session.get('cart', {})
     cart[pk] = cart.get(pk, quantity)
 
     request.session['cart'] = cart
-    return redirect(reverse('index'))
+    return redirect(reverse('view_cart'))
 
 
+@login_required
 def adjust_cart(request, pk):
     """
     Adjust the quantituy of a specified product in the cart
@@ -39,5 +43,3 @@ def adjust_cart(request, pk):
 
     request.session['cart'] = cart
     return redirect(reverse('view_cart'))
-
-# Create your views here.

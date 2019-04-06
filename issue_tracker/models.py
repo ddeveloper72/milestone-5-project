5d5c87d33228b750a52341ffff6641f42312429e
ddeveloper72 <duncan.niamh@gmail.com>
2019-04-07 00:39:46 +0100

    Added a price, decimal field to the issues model use in pricing item

--- issue_tracker/models.py
+++ issue_tracker/models.py
@@ -43,6 +43,8 @@ class Issue(models.Model):
                                 default='BUG')
     votes = models.IntegerField(default=0)
     voter = models.ManyToManyField(User, related_name='issue_upvoters')
+    price = models.DecimalField(
+        max_digits=6, decimal_places=2, default=80.00)
 
     class Meta:
         ordering = ['-created_date']

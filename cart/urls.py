b324775027510528af32d577324384db87eb4755
ddeveloper72 <duncan.niamh@gmail.com>
2019-04-06 23:30:25 +0100

    Refactored urlpatterns to us pk for this project.

--- cart/urls.py
+++ cart/urls.py
@@ -2,7 +2,10 @@ from django.urls import path, re_path
 from cart import views
 
 urlpatterns = [
-    path('', views.view_cart, name='view_cart'),
-    re_path(r'^add/(?P<id>\d+)', views.add_to_cart, name='add_to_cart'),
-    re_path(r'^adjust/(?P<id>\d+)', views.adjust_cart, name='adjust_cart'),
+    path('', views.view_cart,
+         name='view_cart'),
+    re_path(r'^add/(?P<pk>\d+)', views.add_to_cart,
+            name='add_to_cart'),
+    re_path(r'^adjust/(?P<pk>\d+)', views.adjust_cart,
+            name='adjust_cart'),
     ]

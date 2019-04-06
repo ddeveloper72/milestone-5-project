2f44553170df697b9335fbe510653477eac91e50
ddeveloper72 <duncan.niamh@gmail.com>
2019-04-06 21:20:15 +0100

    Imported views from ecommerce project WIP

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+
+# Create your views here.
+
+def view_cart(request):
+    """A view that renders the cart contents page"""
+    return render(request, "cart.html")
+
+
+def add_to_cart(request, id):
+    """Add a quantity of the specified product to the cart"""    
+    quantity=int(request.POST.get('quantity'))    
+    
+    cart = request.session.get('cart', {})
+    cart[id] = cart.get(id, quantity)
+
+    request.session['cart'] = cart
+    return redirect(reverse('index'))
+
+
+def adjust_cart(request, id):
+    """Adjust the quantituy of a specified product in the cart to the speciefied ammount"""
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+
+# we can only adjust a quantity if there is already something in the cart.
+    if quantity > 0:
+        cart[id] = quantity
+    else:
+        cart.pop(id)
+
+    request.session['cart'] = cart
+    return redirect(reverse('view_cart'))
 
 # Create your views here.

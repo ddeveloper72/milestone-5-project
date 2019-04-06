efbd8d8940519e5c751fff4701aab5abdbc606c4
ddeveloper72 <duncan.niamh@gmail.com>
2019-04-06 23:29:35 +0100

    Refactored contexts.py to suit this project.

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,20 +1,21 @@
 from django.shortcuts import get_object_or_404
-from products.models import Product
+from issue_tracker.models import Issue
 
 
 def cart_contents(request):
-    """Ensures that the cart contents are available when rendering every page
+    """
+    Ensures that the cart contents are available when rendering every page
     """
 
     cart = request.session.get('cart', {})
-    
+
     cart_items = []
     total = 0
-    product_count = 0
+    item_count = 0
     for id, quantity in cart.items():
-        product = get_object_or_404(Product, pk=id)
-        total += quantity * product.price
-        product_count += quantity
-        cart_items.append({'id':id, 'quantity': quantity, 'product': product})
-        
-    return { 'cart_items': cart_items, 'total': total, 'product_count': product_count }
+        item = get_object_or_404(Issue, pk=id)
+        total += quantity * item.price
+        item_count += quantity
+        cart_items.append({'id': id, 'quantity': quantity, 'item': item})
+
+    return {'cart_items': cart_items, 'total': total, 'item_count': item_count}

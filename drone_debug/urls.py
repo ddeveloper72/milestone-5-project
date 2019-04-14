55657f65e25eb08d10baa5bd85e1d0f3ed1e55a8
ddeveloper72 <duncan.niamh@gmail.com>
2019-04-14 22:50:33 +0100

    Updated url paths for static files

--- drone_debug/urls.py
+++ drone_debug/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from django.views.generic.base import RedirectView
+from .settings import MEDIA_ROOT
 
 favicon_view = RedirectView.as_view(url='favicon/favicon.ico', permanent=True)
 
@@ -32,4 +34,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('userprofile.urls')),
     path('search/', include('search.urls')),
+    path('media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT})
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

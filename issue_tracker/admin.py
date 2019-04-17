8ced3ff4b26f72a532a06fb4768d6529eeffae8d
ddeveloper72 <duncan.niamh@gmail.com>
2019-04-18 00:00:15 +0100

    Added remaining issue_tracker models to the admin.py

--- issue_tracker/admin.py
+++ issue_tracker/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Issue, Comment
+from .models import Issue, Comment, UserSeenIssue, UserVoted, UserVotedFeature
 
 # Register your models here.
 
 admin.site.register(Issue)
 admin.site.register(Comment)
+admin.site.register(UserSeenIssue)
+admin.site.register(UserVoted)
+admin.site.register(UserVotedFeature)

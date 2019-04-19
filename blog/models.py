0d117799017a6a3ed88a6ef4bfc3ca4ceef00471
ddeveloper72 <duncan.niamh@gmail.com>
2019-04-20 00:13:57 +0100

    Added contenttype fields for storing the object id

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.utils.html import mark_safe
@@ -22,6 +24,9 @@ class Post(models.Model):
     author = models.ForeignKey(User, default=None,
                                related_name="blog_author",
                                on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
 
     class Meta:
         ordering = ['-created_date']

d84cc943566dd3efd92395adff2e7031b0c167b0
ddeveloper72 <duncan.niamh@gmail.com>
2019-04-29 14:18:35 +0100

    Corrected moduel based on errors found at makemigrations.

--- checkout/models.py
+++ checkout/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from issue_tracker.models import Issue
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -14,7 +15,9 @@ class Order(models.Model):
     address_line_2 = models.CharField(max_length=40, blank=False)
     county = models.CharField(max_length=40, blank=False)
     date = models.DateField()
-    buyer = models.ForeignKey(User, default=None)
+    buyer = models.ForeignKey(User, default=None,
+                              related_name="feature_buyer",
+                              on_delete=models.CASCADE)
 
     def __str__(self):
         return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)

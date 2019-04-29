d700db88236614846c69dfd2c5dc2498438fa45c
ddeveloper72 <duncan.niamh@gmail.com>
2019-04-29 14:04:33 +0100

    Added buyer to order model - missing

--- checkout/models.py
+++ checkout/models.py
@@ -14,6 +14,7 @@ class Order(models.Model):
     address_line_2 = models.CharField(max_length=40, blank=False)
     county = models.CharField(max_length=40, blank=False)
     date = models.DateField()
+    buyer = models.ForeignKey(User, default=None)
 
     def __str__(self):
         return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)

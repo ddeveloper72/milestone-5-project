9da97bf3171bb688fad75db749c81ab77d5fc783
ddeveloper72 <duncan.niamh@gmail.com>
2019-12-21 21:30:38 +0000

    Changed local host order for msql db

--- drone_debug/settings.py
+++ drone_debug/settings.py
@@ -112,7 +112,7 @@ else:
             'NAME': 'drone',
             'USER': os.environ.get('USER'),
             'PASSWORD': os.environ.get('PASSWORD'),
-            'HOST': os.environ.get('localhost', '127.0.0.1'),
+            'HOST': os.environ.get('127.0.0.1', 'localhost', ),
             'PORT': os.environ.get('PORT'),
         }
     }

2a91189a0ecb136266143af85318b7a7dafb955d
ddeveloper72 <duncan.niamh@gmail.com>
2020-02-10 19:26:22 +0000

    Added default mySQL localhost for Travis CI

--- drone_debug/settings.py
+++ drone_debug/settings.py
@@ -117,7 +117,7 @@ else:
             'NAME': os.environ.get('MYSQL_DATABASE', 'drone'),
             'USER': os.environ.get('MYSQL_USER'),
             'PASSWORD': os.environ.get('MYSQL_ROOT_PASSWORD'),
-            'HOST': os.environ.get('MYSQL_HOST'),
+            'HOST': os.environ.get('MYSQL_HOST', 'localhost'),
             'PORT': os.environ.get('MYSQL_PORT')
         }
     }

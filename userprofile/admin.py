5e568fa95adbc922698e8d40915b8453e20d46cc
ddeveloper72 <duncan.niamh@gmail.com>
2020-12-01 23:20:53 +0000

    added user profile to admin site

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import UserProfile
 # Register your models here.
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    search_fields = ('bio',)
+    list_display = ('user', 'location', 'bio', )
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
